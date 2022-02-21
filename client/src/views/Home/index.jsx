21cb0ade4b5cc2c7ee6f10624941aff19a6074b4
darkpsinight <darkpsinight@hotmail.com>
2022-02-21 18:17:13 +0100

    adding home + profile

---
+++ client/src/views/Home/index.jsx
@@ -0,0 +1,16 @@
+function Home() {
+    return (
+      <div
+        id='contact'
+        className='contact'
+      >
+        <div className='container'>
+          <div className='row'>
+            <h1>Hello Home</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Home
---
+++ client/src/views/Profile/index.jsx
@@ -0,0 +1,13 @@
+function Profile() {
+    return (
+      <div className='contact'>
+        <div className='container'>
+          <div className='row'>
+            <h1>Hello Profile</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Profile
