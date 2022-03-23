3631c477ce817ee3b63ab318f7887f5cc9860bfa
darkpsinight <darkpsinight@hotmail.com>
2022-03-23 18:50:42 +0100

    23/3/2022

--- client/src/views/Posts/index.jsx
+++ client/src/views/Posts/index.jsx
@@ -83,6 +83,7 @@ export default () => {
                             <Post />
                         </>
                 }
+                
             </div>
 
             <div className="notifications">
