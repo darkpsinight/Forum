10b87d857cc56a01ea4df237c85191619207c0f6
darkpsinight <darkpsinight@hotmail.com>
2022-03-23 20:38:25 +0100

    23/3/2022

--- client/src/views/Home/index.jsx
+++ client/src/views/Home/index.jsx
@@ -8,6 +8,7 @@ function Home() {
                 <div className="row">
                     <div className="col-lg-12 wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.25s">
                         <h1>Hello Home</h1>
+                        <p>Please make an account to be able to see our forum.</p>
                     </div>
                 </div>
             </div>
